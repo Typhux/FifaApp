3c0fb013b2ae6aeca029a6f3087fa536cab1765f
Nowacki <frederic.nowacki@cgm.com>
2020-02-18 09:49:49 +0100

    add bootstrap-vue and firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,36 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+// firebase init goes here
+const config = {
+    apiKey: "AIzaSyDNNsd76tzwUa-W6iuwwtPStuQ1_vNb-TA",
+    databaseURL: "https://fifadb.firebaseio.com",
+    projectId: "fifadb-818a5",
+    messagingSenderId: "715283929582"
+}
+firebase.initializeApp(config)
+
+// firebase utils
+const db = firebase.firestore()
+
+// date issue fix according to firebase
+const settings = {
+    timestampsInSnapshots: true
+}
+db.settings(settings)
+
+// firebase collections
+const teamCollection = db.collection('equipe')
+const playerCollection = db.collection('joueur')
+const leagueCollection = db.collection('ligue')
+const matchCollection = db.collection('match')
+const posteCollection = db.collection('poste')
+
+export {
+    db,
+    teamCollection,
+    playerCollection,
+    leagueCollection,
+    matchCollection,
+    posteCollection
+}
